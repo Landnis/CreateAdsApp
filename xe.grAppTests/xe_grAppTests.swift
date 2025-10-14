97598876bf47efd2315a0f3d1703704ee28584c4
Kostas.Stergiannis <kostas.stergiannis98@gmail.com>
2025-10-14 15:04:57 +0300

    Initial Commit

---
+++ xe.grAppTests/xe_grAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  xe_grAppTests.swift
+//  xe.grAppTests
+//
+//  Created by Konstantinos Stergiannis on 14/10/25.
+//
+
+import Testing
+@testable import xe_grApp
+
+struct xe_grAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
